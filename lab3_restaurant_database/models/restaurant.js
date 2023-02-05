d77211ab9b2a3c0ecf921bd39509bafa76fa9085
bocchuoi <bocchuoi@gmail.com>
2023-02-05 01:15:41 -0500

    model defined

---
+++ lab3_restaurant_database/models/restaurant.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const RestaurantSchema = new mongoose.Schema({
+    "address": [{"building": String, "street": String, "zipcode": String}],
+    "city": String,
+    "cuisine": String,
+    "name": String,
+    "restaurant_id": String
+})
+
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+module.exports = Restaurant
+
+// {
+//     "address": {
+//       "building": "1008",
+//       "street": "Morris Park Ave",
+//       "zipcode": "10462"
+//    },
+//    "city": "Bronx",
+//    "cuisine": "Bakery",
+//    "name": "Morris Park Bake Shop",
+//    "restaurant_id": "30075445"
